443baee7aadb2ac0816fe5fb01e0a844eacc7f38
Cheryl <cheryldownie94@gmail.com>
2019-02-12 19:20:04 -0500

    Full deck

--- lib/blackjack/game.ex
+++ lib/blackjack/game.ex
@@ -2,7 +2,10 @@ defmodule Blackjack.Game do
   
   def new(name) do
     %{
-      letters: Enum.shuffle(["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H"]),
+      letters: Enum.shuffle(["AH", "AS", "AC", "AD", "2H", "2C", "2S", "2D", "3H", "3C", "3S", "3D", "4H", "4C", "4S", "4D",
+                             "5H", "5C", "5S", "5D", "6H", "6C", "6S", "6D", "7H", "7C", "7S", "7D", "8H", "8C", "8S", "8D",
+                             "9H", "9C", "9S", "9D", "0H", "0S", "0C", "0D", "JH", "JC", "JS", "JD", "QH", "QC", "QS", "QD",
+                             "KH", "KC", "KS", "KD"]),
       board: ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
       correct: [],
       name: name,
